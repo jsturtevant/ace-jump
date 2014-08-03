6ee455141e886ae96adbf17248a815c01bfaddd5
James Sturtevant <jsturtevant@gmail.com>
2014-08-03 17:09:39 -0400

    added unit test project and first test

---
+++ AceJumpTests/LetterReferenceDictionaryTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AceJumpTests
+{
+    using AceJump;
+
+    using Microsoft.VisualStudio.Text;
+
+    [TestClass]
+    public class LetterReferenceDictionaryTests
+    {
+        [TestMethod]
+        public void First_Add_Returns_Key_A()
+        {
+            LetterReferenceDictionary letterReferenceDictionary = new LetterReferenceDictionary();
+
+            string key = letterReferenceDictionary.AddSpan(new SnapshotSpan());
+
+            Assert.AreEqual("A", key);
+        }
+    }
+}
