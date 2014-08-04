9ea3cf66e880749c6d8449ad8d9b74301e5e4fc1
James Sturtevant <jsturtevant@gmail.com>
2014-08-04 19:52:13 -0400

    file clean up

--- AceJump/AceJumpFactory.cs
+++ AceJump/AceJumpFactory.cs
@@ -1,29 +1,20 @@
-﻿using System.ComponentModel.Composition;
-using Microsoft.VisualStudio.Text.Editor;
-using Microsoft.VisualStudio.Utilities;
-
-namespace AceJump
+﻿namespace AceJump
 {
     using System.Windows.Input;
+    using System.ComponentModel.Composition;
+    using Microsoft.VisualStudio.Text.Editor;
+    using Microsoft.VisualStudio.Utilities;
 
     [Export(typeof(IKeyProcessorProvider))]
     [ContentType("text")]
     [TextViewRole(PredefinedTextViewRoles.Editable)]
-    [Name("Default Key Processor")]
-   
+    [Name("AceKeyProcessor")]
     internal sealed class AceKeyProcessorProvider : IKeyProcessorProvider
     {
-
         [Export(typeof(AdornmentLayerDefinition))]
         [Name("AceJump")]
         [Order(After = PredefinedAdornmentLayers.Caret)]
         public AdornmentLayerDefinition editorAdornmentLayer = null;
-
-        [ImportingConstructor]
-        internal AceKeyProcessorProvider()
-        {
-         
-        }
 
         public KeyProcessor GetAssociatedProcessor(IWpfTextView wpfTextView)
         {
@@ -34,30 +25,24 @@ namespace AceJump
     public class AceKeyProcessor : KeyProcessor
     {
         private IWpfTextView view;
-
-
         private KeyTypeConverter keyTypeConverter;
-
-
 
         public AceKeyProcessor(IWpfTextView wpfTextView)
         {
             this.view = wpfTextView;
-
             this.keyTypeConverter = new KeyTypeConverter();
         }
-
       
         public override void KeyDown(KeyEventArgs args)
         {
             char? key = this.keyTypeConverter.ConvertToChar(args.Key);
             if (key.HasValue && key.Value == '+')
             {
-
                AceJumperControl aceJumperControl = new AceJumperControl(view);
                aceJumperControl.ShowDialog();  
-
-                args.Handled = true;
+               
+               // mark it handled so it doesn't go down to editor
+               args.Handled = true;
             }
         }
     }
