3e764ce2651db5b8b51e217e55809f90c6031730
James Sturtevant <jsturtevant@gmail.com>
2014-08-06 06:42:02 -0400

    added unit tests for the jump controller

---
+++ AceJumpTests/JumpControlerTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AceJumpTests
+{
+    using AceJump;
+
+    using Moq;
+
+    [TestClass]
+    public class JumpControlerTests
+    {
+        private JumpControler controler;
+
+        private Mock<IAceJumpAdornment> adornmentMock;
+
+        [TestInitialize]
+        public void BeforeEachTest()
+        {
+            this.adornmentMock = new Mock<IAceJumpAdornment>();
+            this.controler = new JumpControler(this.adornmentMock.Object);
+        }
+
+        [TestMethod]
+        public void if_jump_adorment_is_null_return_false()
+        {
+            JumpControler controler = new JumpControler(null);
+            Assert.IsFalse(controler.ControlJump('2'));
+        }
+
+        [TestMethod]
+        public void if_jumper_short_cut_pressed_call_show_jumper()
+        {
+            var handled = controler.ControlJump('+');
+
+            adornmentMock.Verify(a => a.ShowSelector(), Times.Once());
+            Assert.IsTrue(handled);
+        }
+
+        [TestMethod]
+        public void if_jumper_short_cut_pressed_calls_clear_Adroments()
+        {
+            adornmentMock.Setup(a => a.Active).Returns(true);
+            var handled = controler.ControlJump('+');
+
+            adornmentMock.Verify(a => a.ClearAdornments(), Times.Once());
+            Assert.IsTrue(handled);
+        }
+
+        [TestMethod]
+        public void if_Active_and_no_key_passed_ignore_key()
+        {
+            adornmentMock.Setup(a => a.Active).Returns(true);
+            var handled = controler.ControlJump(null);
+
+            Assert.IsTrue(handled);
+        }
+
+        [TestMethod]
+        public void if_second_press_then_jump()
+        {
+            adornmentMock.Setup(a => a.Active).Returns(true);
+
+            // first time activates letters to jump to
+            var handled = controler.ControlJump('a');
+            handled = controler.ControlJump('b');
+
+            adornmentMock.Verify(a=>a.JumpTo("B"), Times.Once);
+            adornmentMock.Verify(a=>a.ClearAdornments(), Times.Once);
+            Assert.IsTrue(handled);
+        }
+
+        [TestMethod]
+        public void if_first_press_then_highlight()
+        {
+            adornmentMock.Setup(a => a.Active).Returns(true);
+
+            // first time activates letters to jump to
+            var handled = controler.ControlJump('a');
+
+            adornmentMock.Verify(a => a.HighlightLetter("A"), Times.Once);
+            Assert.IsTrue(handled);
+        }
+    }
+}
