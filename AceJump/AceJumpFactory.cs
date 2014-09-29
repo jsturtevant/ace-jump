3fa794a7cfce507de29ada1e43b6a160b15f992e
James Sturtevant <jsturtevant@gmail.com>
2014-09-29 07:12:07 -0400

    key processor

--- AceJump/AceJumpFactory.cs
+++ AceJump/AceJumpFactory.cs
@@ -1,9 +1,11 @@
 ﻿namespace AceJump
 {
     using System;
+    using System.Linq;
     using System.Windows.Input;
     using System.ComponentModel.Composition;
 
+    using Microsoft.VisualStudio.Shell.Events;
     using Microsoft.VisualStudio.Text;
     using Microsoft.VisualStudio.Text.Editor;
     using Microsoft.VisualStudio.Utilities;
@@ -31,31 +33,108 @@
 
     public class AceKeyProcessor : KeyProcessor
     {
-        private KeyTypeConverter keyTypeConverter;
+        private KeyConverter keyConverter;
         private JumpControler jumpControler;
 
         public AceKeyProcessor(AceJump aceJump)
         {
-            this.keyTypeConverter = new KeyTypeConverter();
+
             this.jumpControler = new JumpControler(aceJump);
         }
 
+
+
         public override void KeyDown(KeyEventArgs args)
         {
-            if (Keyboard.IsKeyDown(Key.OemSemicolon) && 
-                Keyboard.IsKeyDown(Key.LeftCtrl) && 
-                Keyboard.IsKeyDown(Key.LeftAlt))
+
+            if (Keyboard.IsKeyDown(Key.OemSemicolon) && Keyboard.IsKeyDown(Key.LeftCtrl)
+                && Keyboard.IsKeyDown(Key.LeftAlt))
             {
                 this.jumpControler.ShowJumpEditor();
                 args.Handled = true;
                 return;
             }
-            
-            char? jumpKey = this.keyTypeConverter.ConvertToChar(args.Key);
 
-            bool handled = this.jumpControler.ControlJump(jumpKey);
-            args.Handled = handled;
+            string key = KeyUtility.GetKey(args.Key);
+            if (!string.IsNullOrEmpty(key))
+            {
+                bool handled = this.jumpControler.ControlJump(key.FirstOrDefault());
+                args.Handled = handled;
+            }
+
+        }
+    }
+
+    public class KeyUtility
+    {
+        private static bool IsShiftKey()
+        {
+            return Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift);
+        }
+
+        public static string GetKey(Key key)
+        {
+            var keyConverter = new KeyConverter();
+
+            // converts it to string representation.  IE. Key.E = "E" and Key.OemComma = "OemComma"
+            string character = keyConverter.ConvertToString(key);
+            if (character != null && character.Length == 1)
+            {
+                if (char.IsLetter(character, 0))
+                {
+                    return character;
+                }
+
+                if (char.IsNumber(character, 0) && !IsShiftKey())
+                {
+                    return character;
+                }
+                
+                if (char.IsNumber(character, 0) && IsShiftKey())
+                {
+                    switch (key)
+                    {
+                        // http://msdn.microsoft.com/en-us/library/system.windows.forms.keys(v=vs.110).aspx
+                        case Key.D1:
+                            return "!";
+                        case Key.D2:
+                            return "@";
+                        case Key.D3:
+                            return "#";
+                        case Key.D4:
+                            return "$";
+                        case Key.D5:
+                            return "%";
+                        case Key.D6:
+                            return "^";
+                        case Key.D7:
+                            return "&";
+                        case Key.D8:
+                            return "*";
+                        case Key.D9:
+                            return "(";
+                        case Key.D0:
+                            return ")";
+                    }
+                }
+            }
+
+            switch (key)
+            {
+                // http://msdn.microsoft.com/en-us/library/system.windows.forms.keys(v=vs.110).aspx
+                case Key.Oem4: return IsShiftKey() ? "{" : "[";
+                case Key.Oem6: return IsShiftKey() ? "}" : "]";
+                case Key.Oem5: return IsShiftKey() ? "|" : @"\";
+                case Key.OemMinus: return IsShiftKey() ? "_" : "-";
+                case Key.OemPlus: return IsShiftKey() ? "+" : "=";
+                case Key.OemQuestion: return IsShiftKey() ? "?" : "/";
+                case Key.OemSemicolon: return IsShiftKey() ? ":" : ";";
+                case Key.Oem7: return IsShiftKey() ? "'" : "\"";
+                case Key.OemPeriod: return IsShiftKey() ? ">" : ".";
+                case Key.OemComma: return IsShiftKey() ? "<" : ",";
+            }
+
+            return string.Empty;
         }
     }
 }
-    
